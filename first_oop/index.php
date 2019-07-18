f8cef5de1f023ea905f7e9533f00eb6a5121e4d7
Oskar <oskaras2011@yandex.com>
2019-07-18 11:01:30 +0300

    comments deleted

--- first_oop/index.php
+++ first_oop/index.php
@@ -1,15 +1,5 @@
 <?php
 
-// class Name {
-// 	public $var = 5; // protected and private will not work on line 8
-// }
-
-// $kevin = new Name();
-// print $kevin->var; // do not add $ to var
-
-/**
- * 
- */
 class ThailandSurprise {
 	public $clothes;
 }
