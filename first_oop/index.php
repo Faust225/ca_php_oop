aff596d78c1cea5a1cb7ee74ae9c5e69db898e27
Oskar <oskaras2011@yandex.com>
2019-07-18 10:57:57 +0300

    first oop task

---
+++ first_oop/index.php
@@ -0,0 +1,19 @@
+<?php
+
+// class Name {
+// 	public $var = 5; // protected and private will not work on line 8
+// }
+
+// $kevin = new Name();
+// print $kevin->var; // do not add $ to var
+
+/**
+ * 
+ */
+class ThailandSurprise {
+	public $clothes;
+}
+
+$val = new ThailandSurprise();
+$val->clothes = 'miniskirt';
+print $val->clothes;
