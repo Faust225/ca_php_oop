630bac56d04c7588a2d5c7dc5bf33b225f0629e5
Oskar <oskaras2011@yandex.com>
2019-07-23 13:20:44 +0300

    cash removed from ->$

--- set_get/index.php
+++ set_get/index.php
@@ -4,37 +4,37 @@ class Drink {
     private $data = [];
 
     public function setName(string $name) {
-        $this->$data['name'] = $name;
+        $this->data['name'] = $name;
     }
 
     public function setAmountMl(int $amount_ml) {
-        $this->$data['amount'] = $amount_ml;
+        $this->data['amount'] = $amount_ml;
     }
 
     public function setAbarot(float $abarot) {
-        $this->$data['abarot'] = $abarot;
+        $this->data['abarot'] = $abarot;
     }
 
     public function setImage(string $image) {
-        $this->$data['image'] = $image;
+        $this->data['image'] = $image;
     }
 
     /**
      * gets starts
      */
     public function getName() {
-       return $this->$data['name'];
+       return $this->data['name'];
     }
 
     public function getAmountMl() {
-        return $this->$data['amount'];
+        return $this->data['amount'];
     }
 
     public function getAbarot() {
-        return $this->$data['abarot'];
+        return $this->data['abarot'];
     }
 
     public function getImage() {
-        return $this->$data['image'];
+        return $this->data['image'];
     }
 }
